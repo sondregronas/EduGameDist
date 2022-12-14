02b61b391d9e389fb06f49735323c16bf5619882
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-12-14 19:50:39 +0100

    :lipstick: Beautify code, simplify getGameList

--- src/server.js
+++ src/server.js
@@ -9,39 +9,56 @@ const app = express()
 
 app.use(express.static('public'))
 app.set('view engine', 'pug')
-const pugData = {title: process.env.TITLE || 'Game Distribution Platform'}
+const pugData = {
+    title: process.env.TITLE || 'Game Distribution Platform'
+}
 
 // Database
 util.serializeDB(db_file)
 
 // Environment Variables
-if (process.env.DOCKER) { util.copyFolderSync('/app.defaults', '/app')}
-if (process.env.NODE_ENV === 'dev') { util.devMode(app) }
+if (process.env.DOCKER) {
+    util.copyFolderSync('/app.defaults', '/app')
+}
+if (process.env.NODE_ENV === 'dev') {
+    util.devMode(app)
+}
 
 app.get('/*', async (req, res) => {
-  let url = req.url.replace(/^\/+/, '')
-  let base_url = req.protocol + '://' + req.get('host')
-  let abs_url = base_url + req.originalUrl
+    let url = req.url.replace(/^\/+/, '')
+    let base_url = req.protocol + '://' + req.get('host')
+    let abs_url = base_url + req.originalUrl
 
-  if (pugData.gameList[url]) { pugData.gameData = pugData.gameList[url];
-                               pugData.gameData.base_url = base_url;
-                               pugData.gameData.abs_url = abs_url;
-                               url = 'game' }
-  else if (url === '') { url = 'index' }
+    if (pugData.gameList[url]) {
+        pugData.gameData = pugData.gameList[url];
+        pugData.gameData.base_url = base_url;
+        pugData.gameData.abs_url = abs_url;
+        url = 'game'
+    } else if (url === '') {
+        url = 'index'
+    }
 
-  try {
-    res.render(url, pugData, (err, html) => {
-      if (err) { throw(err) }
-      else { res.send(html) }
-    })
-  } catch (err) { console.error(err); res.redirect(`/`) }
+    try {
+        res.render(url, pugData, (err, html) => {
+            if (err) {
+                throw (err)
+            } else {
+                res.send(html)
+            }
+        })
+    } catch (err) {
+        console.error(err);
+        res.redirect(`/`)
+    }
 })
 
 // Update game list every 15 minutes in the background
-setInterval(() => { pugData.gameList = util.getGameList(db_file, pugData) }, 1000 * 60 * 15)
+setInterval(() => {
+    pugData.gameList = util.getGameList(db_file, pugData)
+}, 1000 * 60 * 15)
 
 // Start app
 app.listen(port, () => {
-  pugData.gameList = util.getGameList(db_file, pugData)
-  console.log(`Listening on port ${port}`)
+    pugData.gameList = util.getGameList(db_file, pugData)
+    console.log(`Listening on port ${port}`)
 })
